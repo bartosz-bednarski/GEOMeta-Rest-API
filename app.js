1a2ec0078a32ac4ef57f6ef068c5c0f922d2d85a
Bartosz Bednarski <bambocha1997@gmail.com>
2023-08-23 18:50:58 +0200

    fix3

--- app.js
+++ app.js
@@ -3,14 +3,30 @@ const bodyParser = require("body-parser");
 const continentsRoutes = require("./routes/continents-routes");
 const cors = require("cors");
 const app = express();
-app.use(
-  cors({
-    origin: ["https://geo-meta.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    credentials: true,
-    "Access-Control-Allow-Origin": "*",
-  })
-);
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "https://geo-meta.vercel.app");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 app.use("/api/continents", continentsRoutes);
 
 app.listen("https://geo-meta-rest-api.vercel.app");
