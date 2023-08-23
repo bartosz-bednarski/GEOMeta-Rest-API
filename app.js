6ec52c2d15ac1ee8d3301ffb3f629879a1791ac8
Bartosz Bednarski <bambocha1997@gmail.com>
2023-08-23 18:40:00 +0200

    fix2

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 app.use(
   cors({
-    origin: ["https://geo-meta.vercel.app/continents"],
+    origin: ["https://geo-meta.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
     "Access-Control-Allow-Origin": "*",
@@ -13,4 +13,4 @@ app.use(
 );
 app.use("/api/continents", continentsRoutes);
 
-// app.listen("geo-meta-rest-api.vercel.app");
+app.listen("https://geo-meta-rest-api.vercel.app");
