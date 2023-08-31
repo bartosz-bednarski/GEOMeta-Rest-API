beb9172431d5375d9d70d80e2dcef50465a9e1fe
Bartosz Bednarski <bambocha1997@gmail.com>
2023-08-31 19:42:02 +0200

    install dotenv

--- config.js
+++ config.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
 const config = {
   db: {
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     connectTimeout: 60000,
+    port: process.env.DB_PORT,
   },
   listPerPage: 10,
 };
