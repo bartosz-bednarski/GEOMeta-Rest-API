64af7d73ab43a32d323334848da8b4d815a5ce45
Bartosz Bednarski <bambocha1997@gmail.com>
2023-10-24 14:46:12 +0200

    Comments handling and topics

--- controllers/forum-controllers.js
+++ controllers/forum-controllers.js
@@ -16,16 +16,25 @@ var month = [
   "Lis",
   "Gru",
 ];
-let date = new Date();
-let day = new String(date.getDate());
-day = day.length === 1 ? `0${day}` : day;
-let hours = new String(date.getHours());
-hours = hours.length === 1 ? `0${hours}` : hours;
-let minutes = new String(date.getMinutes());
-minutes = minutes.length === 1 ? `0${minutes}` : minutes;
-let dateExpo = day + " " + month[date.getMonth()] + " " + date.getFullYear();
-let timeExpo = hours + ":" + minutes;
+// let date = new Date();
+// let day = new String(date.getDate());
+// day = day.length === 1 ? `0${day}` : day;
+// let hours = new String(date.getHours());
+// hours = hours.length === 1 ? `0${hours}` : hours;
+// let minutes = new String(date.getMinutes());
+// minutes = minutes.length === 1 ? `0${minutes}` : minutes;
+// let dateExpo = day + " " + month[date.getMonth()] + " " + date.getFullYear();
+// let timeExpo = hours + ":" + minutes;
 const createTopic = async (req, res, next) => {
+  let date = new Date();
+  let day = new String(date.getDate());
+  day = day.length === 1 ? `0${day}` : day;
+  let hours = new String(date.getHours());
+  hours = hours.length === 1 ? `0${hours}` : hours;
+  let minutes = new String(date.getMinutes());
+  minutes = minutes.length === 1 ? `0${minutes}` : minutes;
+  let dateExpo = day + " " + month[date.getMonth()] + " " + date.getFullYear();
+  let timeExpo = hours + ":" + minutes;
   const data = [
     {
       topic_id: uuidv1(),
@@ -35,6 +44,9 @@ const createTopic = async (req, res, next) => {
       dateString: dateExpo,
       timeString: timeExpo,
       date: new Date(),
+      comments: [],
+      latestUpdate: dateExpo,
+      iconBackgroundColor: req.body.iconBackgroundColor,
     },
   ];
   try {
@@ -59,6 +71,7 @@ const getTopics = async (req, res, next) => {
     const db = client.db("Geometa");
     const forumCollection = db.collection("Topics");
     const response = await forumCollection.find({}).toArray();
+
     res.status(201).json({ message: "Success", data: response });
     client.close();
   } catch (err) {
@@ -67,6 +80,15 @@ const getTopics = async (req, res, next) => {
   }
 };
 const createComment = async (req, res, next) => {
+  let date = new Date();
+  let day = new String(date.getDate());
+  day = day.length === 1 ? `0${day}` : day;
+  let hours = new String(date.getHours());
+  hours = hours.length === 1 ? `0${hours}` : hours;
+  let minutes = new String(date.getMinutes());
+  minutes = minutes.length === 1 ? `0${minutes}` : minutes;
+  let dateExpo = day + " " + month[date.getMonth()] + " " + date.getFullYear();
+  let timeExpo = hours + ":" + minutes;
   let data;
   if (req.user === undefined) {
     data = [
@@ -107,6 +129,29 @@ const createComment = async (req, res, next) => {
     const forumCollection = db.collection("Comments");
     const response = await forumCollection.insertMany(data);
     client.close();
+    const client2 = await mongo.MongoClient.connect(
+      `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.kl5um6i.mongodb.net/?retryWrites=true&w=majority`
+    );
+    const topicsCollection = client2.db("Geometa").collection("Topics");
+    console.log("data", data[0].usernameShort);
+    const update = await topicsCollection.findOneAndUpdate(
+      {
+        topic_id: req.body.topic_id,
+      },
+
+      {
+        $push: {
+          comments: {
+            iconBackgroundColor: data[0].iconBackgroundColor,
+            usernameShort: data[0].usernameShort,
+            date: data[0].dateString,
+            time: data[0].timeString,
+          },
+        },
+      },
+      { new: true }
+    );
+    client2.close();
     res.status(201).json({ message: "Success" });
   } catch (err) {
     console.log(err);
