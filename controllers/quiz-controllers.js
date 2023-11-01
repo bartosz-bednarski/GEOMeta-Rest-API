4aef436d9e35e31bf27451f51f178e6ca980e45c
Bartosz Bednarski <bambocha1997@gmail.com>
2023-11-01 14:45:28 +0100

    Fixed postFlags-quiz handler

--- controllers/quiz-controllers.js
+++ controllers/quiz-controllers.js
@@ -16,12 +16,13 @@ const getFlags = async (req, res, next) => {
       response.push({ data: dataWithId });
     }
     for (let x = 0; x < 5; x++) {
-      let answer = Math.round(Math.random() * (3 - 1) + 1);
+      let answer = Math.round(Math.random() * (3 - 1) + 1).toString();
       let question = response[x].data[answer].country_name;
+      console.log(await bcrypt.hash(answer, 10));
       response[x] = {
         ...response[x],
         id: x,
-        answer: jwt.sign(answer, process.env.QUIZ_ANSWER_TOKEN_SECRET),
+        answer: await bcrypt.hash(answer, 10),
         question: question,
       };
       for (let j = 0; j < 4; j++) {
@@ -31,7 +32,46 @@ const getFlags = async (req, res, next) => {
 
     res.status(201).json({ message: "Success", data: response });
   } catch (err) {
+    console.log(err);
     return next(new HttpError("Failed to connect with db!"), 500);
   }
 };
-module.exports = { getFlags };
+const postFlags = async (req, res, next) => {
+  try {
+    const data = req.body;
+    let userScore = 0;
+    const userCorrectAnswers = [];
+
+    for (let i = 0; i < data.length; i++) {
+      if (
+        (await bcrypt.compare(data[i].user_answer, data[i].correct_answer)) ==
+        true
+      ) {
+        userCorrectAnswers.push({
+          question_number: i,
+          userAnswer: "correct",
+        });
+        userScore += 1;
+      }
+      if (
+        (await bcrypt.compare(data[i].user_answer, data[i].correct_answer)) ==
+        false
+      ) {
+        userCorrectAnswers.push({
+          question_number: i,
+          userAnswer: "wrong",
+        });
+      }
+    }
+    res.status(201).json({
+      message: "ok",
+      data: {
+        user_score: userScore,
+        user_correct_answers: userCorrectAnswers,
+      },
+    });
+  } catch (err) {
+    return next(new HttpError("Failed to connect with db!"), 500);
+  }
+};
+module.exports = { getFlags, postFlags };
--- routes/quiz-routes.js
+++ routes/quiz-routes.js
@@ -3,5 +3,5 @@ const quizControllers = require("../controllers/quiz-controllers");
 const router = express.Router();
 
 router.get("/getFlags", quizControllers.getFlags);
-
+router.post("/postFlags", quizControllers.postFlags);
 module.exports = router;
