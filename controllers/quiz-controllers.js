4d057bf0b9f07ecb1a6224f2beabe8ab7de64a57
Bartosz Bednarski <bambocha1997@gmail.com>
2023-11-02 12:57:22 +0100

    Added emblems-quiz controllers

--- controllers/quiz-controllers.js
+++ controllers/quiz-controllers.js
@@ -4,6 +4,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const db = require("./db");
 
+//Flags
 const getFlags = async (req, res, next) => {
   try {
     const rows = await db.query(
@@ -66,7 +67,7 @@ const postFlags = async (req, res, next) => {
     res.status(201).json({
       message: "ok",
       data: {
-        user_score: userScore,
+        user_score: userScore * 10,
         user_correct_answers: userCorrectAnswers,
       },
     });
@@ -74,4 +75,173 @@ const postFlags = async (req, res, next) => {
     return next(new HttpError("Failed to connect with db!"), 500);
   }
 };
-module.exports = { getFlags, postFlags };
+const postFlagsAuth = async (req, res, next) => {
+  const user = req.user.username;
+  try {
+    const data = req.body;
+    let userScore = 0;
+    const userCorrectAnswers = [];
+
+    for (let i = 0; i < data.length; i++) {
+      if (
+        (await bcrypt.compare(data[i].user_answer, data[i].correct_answer)) ==
+        true
+      ) {
+        userCorrectAnswers.push({
+          question_number: i,
+          userAnswer: "correct",
+        });
+        userScore += 1;
+      }
+      if (
+        (await bcrypt.compare(data[i].user_answer, data[i].correct_answer)) ==
+        false
+      ) {
+        userCorrectAnswers.push({
+          question_number: i,
+          userAnswer: "wrong",
+        });
+      }
+    }
+    const rows = await db.query(
+      `update users set flags_quiz_score=(select flags_quiz_score from users where username='${user}')+${
+        userScore * 10
+      } where username='${user}'`
+    );
+    res.status(201).json({
+      message: "ok",
+      data: {
+        user_score: userScore * 10,
+        user_correct_answers: userCorrectAnswers,
+      },
+    });
+  } catch (err) {
+    return next(new HttpError("Failed to connect with db!"), 500);
+  }
+};
+
+//Emblems
+
+const getEmblems = async (req, res, next) => {
+  try {
+    const rows = await db.query(
+      "select country_name, emblem from country_details order by RANDOM() limit 20"
+    );
+    const data = rows;
+    let response = [];
+    for (let i = 0; i <= 16; i += 4) {
+      let dataWithId = data.slice(i, i + 4);
+      response.push({ data: dataWithId });
+    }
+    for (let x = 0; x < 5; x++) {
+      let answer = Math.round(Math.random() * (3 - 1) + 1).toString();
+      let question = response[x].data[answer].country_name;
+      response[x] = {
+        ...response[x],
+        id: x,
+        answer: await bcrypt.hash(answer, 10),
+        question: question,
+      };
+      for (let j = 0; j < 4; j++) {
+        delete response[x].data[j].country_name;
+      }
+    }
+
+    res.status(201).json({ message: "Success", data: response });
+  } catch (err) {
+    console.log(err);
+    return next(new HttpError("Failed to connect with db!"), 500);
+  }
+};
+const postEmblems = async (req, res, next) => {
+  try {
+    const data = req.body;
+    let userScore = 0;
+    const userCorrectAnswers = [];
+
+    for (let i = 0; i < data.length; i++) {
+      if (
+        (await bcrypt.compare(data[i].user_answer, data[i].correct_answer)) ==
+        true
+      ) {
+        userCorrectAnswers.push({
+          question_number: i,
+          userAnswer: "correct",
+        });
+        userScore += 1;
+      }
+      if (
+        (await bcrypt.compare(data[i].user_answer, data[i].correct_answer)) ==
+        false
+      ) {
+        userCorrectAnswers.push({
+          question_number: i,
+          userAnswer: "wrong",
+        });
+      }
+    }
+    res.status(201).json({
+      message: "ok",
+      data: {
+        user_score: userScore * 10,
+        user_correct_answers: userCorrectAnswers,
+      },
+    });
+  } catch (err) {
+    return next(new HttpError("Error server!"), 500);
+  }
+};
+
+const postEmblemsAuth = async (req, res, next) => {
+  const user = req.user.username;
+  try {
+    const data = req.body;
+    let userScore = 0;
+    const userCorrectAnswers = [];
+
+    for (let i = 0; i < data.length; i++) {
+      if (
+        (await bcrypt.compare(data[i].user_answer, data[i].correct_answer)) ==
+        true
+      ) {
+        userCorrectAnswers.push({
+          question_number: i,
+          userAnswer: "correct",
+        });
+        userScore += 1;
+      }
+      if (
+        (await bcrypt.compare(data[i].user_answer, data[i].correct_answer)) ==
+        false
+      ) {
+        userCorrectAnswers.push({
+          question_number: i,
+          userAnswer: "wrong",
+        });
+      }
+    }
+    const rows = await db.query(
+      `update users set emblems_quiz_score=(select emblems_quiz_score from users where username='${user}')+${
+        userScore * 10
+      } where username='${user}'`
+    );
+    console.log(rows);
+    res.status(201).json({
+      message: "ok",
+      data: {
+        user_score: userScore * 10,
+        user_correct_answers: userCorrectAnswers,
+      },
+    });
+  } catch (err) {
+    return next(new HttpError("Failed to connect with db!"), 500);
+  }
+};
+module.exports = {
+  getFlags,
+  postFlags,
+  postFlagsAuth,
+  getEmblems,
+  postEmblems,
+  postEmblemsAuth,
+};
--- routes/quiz-routes.js
+++ routes/quiz-routes.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const usersControllers = require("../controllers/users-controllers");
 const quizControllers = require("../controllers/quiz-controllers");
 const router = express.Router();
 
 router.get("/getFlags", quizControllers.getFlags);
 router.post("/postFlags", quizControllers.postFlags);
+router.post(
+  "/postFlags/auth",
+  usersControllers.authenticateToken,
+  quizControllers.postFlagsAuth
+);
+router.get("/getEmblems", quizControllers.getEmblems);
+router.post("/postEmblems", quizControllers.postEmblems);
+router.post(
+  "/postEmblems/auth",
+  usersControllers.authenticateToken,
+  quizControllers.postEmblemsAuth
+);
 module.exports = router;
