caa0eb257e6c7abf890b60d616406a6f7e7ba48a
Bartosz Bednarski <bambocha1997@gmail.com>
2023-11-05 13:30:28 +0100

    Fixed changePassword handler. Added change usernameShort and get profile handlers.

--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -2,10 +2,19 @@ const express = require("express");
 const usersControllers = require("../controllers/users-controllers");
 const profileControllers = require("../controllers/profile-controllers");
 const router = express.Router();
-
+router.get(
+  "/getProfile",
+  usersControllers.authenticateToken,
+  profileControllers.getProfile
+);
 router.post(
   "/changePassword",
   usersControllers.authenticateToken,
   profileControllers.changePassword
+);
+router.post(
+  "/changeShortName",
+  usersControllers.authenticateToken,
+  profileControllers.changeShortname
 );
 module.exports = router;
